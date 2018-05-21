fe151efdd13cca92711b0967406cf928b19c93f0
Jimmy-Anderson <roshankushaw1@gmail.com>
2018-05-21 19:20:52 +0530

    Bugs in homepage fixed

--- js/home.js
+++ js/home.js
@@ -9,28 +9,46 @@ var s=str.replace(" ",", ");
 document.getElementById("demo").innerHTML = s;
 
 //Main menu for small screens(<768px)
+
+
 function menuexpand(x){
 	 x.classList.toggle("change");
-	var x=document.getElementsByClassName("nav-item");
-	document.getElementsByClassName("navbar-nav")[0].style.flexDirection="column";
+	var y=document.getElementsByClassName("nav-item");
 	var i;
-	document.getElementsByClassName("navbar-nav")[0].classList.toggle("activebtn");
-	for(i=0;i<x.length;i++)
+	for(i=0;i<y.length;i++)
 	{	
-		x[i].classList.toggle("show");
+		y[i].classList.toggle("show");
 	}
 }
 window.onclick = function(event) {
   if (!event.target.matches('.optionbtn')) {
 
     var dropdowns = document.getElementsByClassName("nav-item");
+    var menuicon = document.getElementsByClassName("optionbtn");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        document.getElementsByClassName("navbar-nav")[0].style.flexDirection="row";
+        openDropdown.classList.remove('show');  
       }
+      if(menuicon[0].classList.contains('change'))
+        menuicon[0].classList.remove('change');   
     }
   }
 }
+
+//Navbar fixing at the top
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var navfix=document.getElementsByClassName("navbar");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        navfix[0].id = "navbar-fixed";
+    }
+    else{
+        navfix[0].id = "";
+    }
+    
+}
+
